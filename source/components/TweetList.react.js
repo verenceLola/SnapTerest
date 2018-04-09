4dd491b46c0c6df6b7853f8f54d600660939cb71
Vincent Onyango <verencelola@gmail.com>
2018-04-09 05:30:34 +0300

    Added components for managing the incoming tweets and displaying them on a list

---
+++ source/components/TweetList.react.js
@@ -0,0 +1,34 @@
+var React = require('react');
+var Tweet = require('./Tweet.react');
+var listStyle = {
+    padding: '0'
+};
+var listItemStyle ={
+    display: 'inline-block',
+    listStyle: 'none'
+};
+var TweetList = React.createClass({
+    getListOfTweetIds: function(){
+        return Object.keys(this.props.tweets);
+    },
+    getTweetElement: function(tweetId){
+        var tweet = this.props.tweets[tweetId];
+        var handleRemoveTweetFromeCollection = this.props.onRemoveTweetFromCollection;
+        var tweetElement;
+        if (handleRemoveTweetFromeCollection) {
+            tweetElement=(
+                <Tweet tweet={tweet} onImageClick={handleRemoveTweetFromeCollection}/>
+            );
+        }else{
+            tweetElement = <Tweet tweet={tweet}/>;
+        }
+        return <li style={listStyle} key={tweet.id}>{tweetElement}</li>;
+    },
+    render: function(){
+        var tweetElements = this.getListOfTweetIds().map(this.getTweetElement);
+        return (
+            <ul style={listStyle}>{tweetElements}</ul>
+        );
+    }
+});
+module.exports =TweetList;
