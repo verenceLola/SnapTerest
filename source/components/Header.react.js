ed4c9918c7d9fe1ad0d0bc7cef5de8852b890c06
Vincent Onyango <verencelola@gmail.com>
2018-04-09 05:28:13 +0300

    Added Header.react.js component for displaying the header information of the collected tweets such as the number of new tweets and the tweets currently being displayed

---
+++ source/components/Header.react.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var headerStyle= {
+    fontSize: '16px',
+    fontWeight: '300',
+    display: 'inline-block',
+    margin: '20px 10px'
+};
+var Header= React.createClasss({
+    getDefaultProps: function(){
+        return {
+            text: 'Default header'
+        };
+    },
+    render: function(){
+        return(
+            <h2 style={headerStyle}>{this.props.text}</h2>
+        );
+    }
+});
+module.exports = Header;
